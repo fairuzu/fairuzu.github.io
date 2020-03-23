511c2fcb80a1a276525e2e61bd2fa22a3973a66f
Fairuz <fgrweleh007@gmail.com>
2020-03-24 00:54:38 +0700

    Add files via upload

---
+++ precache-manifest.70bcf2e5c29bcb3086416999c84e71ad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4d045c7f402a1e287ae6122540e09c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06495bb2535fb6939f01",
+    "url": "/static/css/main.9279d183.chunk.css"
+  },
+  {
+    "revision": "bc15d8fc7cf1f0d8bb20",
+    "url": "/static/js/2.3259d60a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.3259d60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06495bb2535fb6939f01",
+    "url": "/static/js/main.f359dcef.chunk.js"
+  },
+  {
+    "revision": "541cbbcdedce3c3f89e0",
+    "url": "/static/js/runtime-main.959b4b4d.js"
+  },
+  {
+    "revision": "327881215df3d28ca39939fc848dbb09",
+    "url": "/static/media/Fairuz-kun_logo_2020_4_512px.32788121.png"
+  },
+  {
+    "revision": "7dbcb9b2b1d873e327e0c6f9ed5b58ab",
+    "url": "/static/media/FakeAnime_001.7dbcb9b2.jpg"
+  },
+  {
+    "revision": "6cc22b3fc06894f415e8ab6e16838eef",
+    "url": "/static/media/Fanart_5toubun_yotsuba.6cc22b3f.jpg"
+  },
+  {
+    "revision": "24d4cc1981943708f7133d4ed1d40b9c",
+    "url": "/static/media/Fanart_Cytus II_Æsir_1080p.24d4cc19.jpg"
+  },
+  {
+    "revision": "d6f82f903bd064e34c6926b253d63cf6",
+    "url": "/static/media/Frbdn_timeline_1080p.d6f82f90.jpg"
+  },
+  {
+    "revision": "31eef4a85429b5c7f7bda1a3b47f3804",
+    "url": "/static/media/Furubi_06.31eef4a8.jpg"
+  },
+  {
+    "revision": "1195891690004ce3283fdc20d470d9c2",
+    "url": "/static/media/Furubi_07.11958916.jpg"
+  },
+  {
+    "revision": "b296be7744f10c4a26486a4438de89d2",
+    "url": "/static/media/Furubi_08.b296be77.jpg"
+  },
+  {
+    "revision": "91c90f733a94dff1d1ed594ead5a16b8",
+    "url": "/static/media/Logitech_F.91c90f73.jpg"
+  },
+  {
+    "revision": "06e3b14ca0925e23e084fd3f208622ae",
+    "url": "/static/media/fairu-zu_project_X7_RGB.06e3b14c.jpg"
+  }
+]);
