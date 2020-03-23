fb1951f116aeeb4a856b44a22963927d29a46fa1
Fairuz <fairuz.gh.r@gmail.com>
2020-03-23 21:25:17 +0700

    Updates

---
+++ precache-manifest.e3c92a68a1b4d03d0d39300222c7ed5e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4159d837d07badc5a32bd323404247a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a675796a245df4dd6a0b",
+    "url": "/static/css/main.0fd2f7a8.chunk.css"
+  },
+  {
+    "revision": "3a2d087da48d0a970c32",
+    "url": "/static/js/2.7d2309d1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.7d2309d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a675796a245df4dd6a0b",
+    "url": "/static/js/main.650faea6.chunk.js"
+  },
+  {
+    "revision": "14e33ad4ab6549f4b83f",
+    "url": "/static/js/runtime-main.bc01f412.js"
+  },
+  {
+    "revision": "c1d31aa42686488b1d2c21f18b6df730",
+    "url": "/static/media/Fairuz-kun_logo_2018_2.c1d31aa4.png"
+  },
+  {
+    "revision": "327881215df3d28ca39939fc848dbb09",
+    "url": "/static/media/Fairuz-kun_logo_2020_4_512px.32788121.png"
+  },
+  {
+    "revision": "32e90642852cf1594c1e41de57e6a99d",
+    "url": "/static/media/FakeAnime_001.32e90642.jpg"
+  },
+  {
+    "revision": "0b3b86fc11bc347335240057553d9fc5",
+    "url": "/static/media/Fanart_Cytus II_Æsir_1080p.0b3b86fc.png"
+  },
+  {
+    "revision": "6ccd3f95788611b12d12027a13f9d850",
+    "url": "/static/media/Frbdn_timeline_1080p.6ccd3f95.jpg"
+  },
+  {
+    "revision": "74f0c7eff16fd30d0a03b5b46c7e6cf1",
+    "url": "/static/media/Furubi_06.74f0c7ef.png"
+  },
+  {
+    "revision": "2dc3e9a72bd92c7a5925b0eb19c12484",
+    "url": "/static/media/Logitech_F.2dc3e9a7.png"
+  },
+  {
+    "revision": "06e3b14ca0925e23e084fd3f208622ae",
+    "url": "/static/media/fairu-zu_project_X7_RGB.06e3b14c.jpg"
+  }
+]);
